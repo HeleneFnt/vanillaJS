35eb2c3b90a462dead8792005a88d69ee8da7c50
LNWatta <helene.finot@le-campus-numerique.fr>
2024-04-02 16:44:29 +0200

    refactor page

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,21 @@
-
-fetch("https://tyradex.tech/api/v1/pokemon")
+const randomIndex = Math.floor(Math.random() * 100);
+fetch("https://tyradex.tech/api/v1/pokemon/"+ randomIndex)
     .then(response => response.json()) // transforme le résultat de l'API en JSON
-    .then(data => {
+    .then(pokemon => {
 
-// Affichage des données des Pokémon
-let pokemonIdElement = document.getElementById('pokemon_id');
-let pokemonNameElement = document.getElementById('pokemon_category');
-let pokemonDescriptionElement = document.getElementById('pokemon_name');
-let pokemonPictureElement = document.getElementById('pokemon_picture');
+        // Affichage des données des Pokémon
+        let pokemonIdElement = document.getElementById('pokemon_id');
+        let pokemonCategoryElement = document.getElementById('pokemon_category');
+        let pokemonNameElement = document.getElementById('pokemon_name');
+        let pokemonPictureElement = document.getElementById('pokemon_picture');
 
+        // Mise à jour des div avec les données du premier Pokémon
+        pokemonIdElement.textContent = "ID: #" + pokemon.pokedex_id;
+        pokemonCategoryElement.textContent = "Type: " + pokemon.category;
+        pokemonNameElement.textContent = "Nom: " + pokemon.name.fr;
+        pokemonPictureElement.src = pokemon.sprites.regular;
+    })
+    .catch(error => {
+        console.error('Une erreur s\'est produite:', error);
+    });
 
-// Récupération des données du premier Pokémon
-const firstPokemon = data[1];
-
-// Mise à jour des div avec les données du premier Pokémon
-pokemonIdElement.textContent = "ID: #" + firstPokemon.pokedex_id;
-pokemonNameElement.textContent = "Type: " + firstPokemon.category;
-pokemonDescriptionElement.textContent = "Description: " + firstPokemon.name.fr;
-pokemonPictureElement.src = firstPokemon.sprites.regular;
-})
-.catch(error => {
-    console.error('Une erreur s\'est produite:', error);
-});
