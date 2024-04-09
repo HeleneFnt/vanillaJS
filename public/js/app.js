67d757cb757b1b80c58927b761a427241795b277
LNWatta <helene.finot@le-campus-numerique.fr>
2024-04-09 10:29:23 +0200

    fix form

--- public/js/app.js
+++ public/js/app.js
@@ -20,22 +20,44 @@ for (let i = 0; i < 3; i++) {
 
 
 // Création du formulaire pour ajouter un pokémon
-const form = document.createElement('div');
+const form = document.createElement('form');
 form.id = 'pokemon_form';
+
+// Création des labels et champs de formulaire
+const categoryLabel = document.createElement('label');
+categoryLabel.textContent = 'Type:';
+categoryLabel.setAttribute('for', 'category_input');
+form.appendChild(categoryLabel);
 
 const categoryInput = document.createElement('input');
 categoryInput.type = 'text';
 categoryInput.placeholder = 'Type';
+categoryInput.required = true; // Champ requis
+categoryInput.id = 'category_input';
 form.appendChild(categoryInput);
+
+const nameLabel = document.createElement('label');
+nameLabel.textContent = 'Nom:';
+nameLabel.setAttribute('for', 'name_input');
+form.appendChild(nameLabel);
 
 const nameInput = document.createElement('input');
 nameInput.type = 'text';
 nameInput.placeholder = 'Nom';
+nameInput.required = true; // Champ requis
+nameInput.id = 'name_input';
 form.appendChild(nameInput);
+
+const imageLabel = document.createElement('label');
+imageLabel.textContent = 'URL du Pokémon:';
+imageLabel.setAttribute('for', 'image_input');
+form.appendChild(imageLabel);
 
 const imageInput = document.createElement('input');
 imageInput.type = 'text';
 imageInput.placeholder = 'URL du Pokémon'; // https://img-4.linternaute.com/r8EjCC8ozJAzUlArRnxEiHX4yPU=/750x1125/smart/63ec5f73fbff4975a6e42e4ec973fef0/ccmcms-linternaute/animal-etrange-1.jpg
+imageInput.required = true; // Champ requis
+imageInput.id = 'image_input';
 form.appendChild(imageInput);
 
 const addButton = document.createElement('button');
@@ -51,6 +73,13 @@ addButton.addEventListener('click', function () {
     const category = categoryInput.value;
     const name = nameInput.value;
     const image = imageInput.value;
+
+
+    // Vérification si les champs requis sont remplis
+    if (!category || !name || !image) {
+        alert('Veuillez remplir tous les champs requis.');
+        return; // Arrête la fonction si un champ requis est vide
+    }
 
     // Création du nouvel objet Pokémon avec les données fournies par l'utilisateur
     const pokemon = createPokemon(category, name, image);
@@ -80,9 +109,8 @@ function createPokemon(category, name, image) {
     };
 }
 
-// Fonction pour supprimer le Pokémon crée
+// Fonction pour supprimer le Pokémon créé
 function deleteCard(event) {
     const cardToRemove = event.target.closest('.card_add');
     cardToRemove.remove();
 }
-
